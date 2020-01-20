02995954244cacee86a17e285c88ca8f4d11b980
kamper541 <kamper543@gmail.com>
2020-01-20 22:07:47 +0700

    test

---
+++ calculation.js
@@ -0,0 +1,44 @@
+// let x = document.getElementsByClassName("form-control");
+// //var y = parseInt("x");
+
+// console.log(x);
+var ans = Math.floor(Math.random() * 100) + 1;
+var element, newElement, parent;
+var temp = 0;
+document.forms["calculation"].onsubmit = function(){
+    console.log(ans);
+    var c = document.getElementById("want").value;
+    if(temp == 10){
+        var para = document.createElement("p"); 
+                        // Create a <p> element
+        para.innerHTML = "You Lose!!!! ...Staring new Game";                // Insert text
+        temp = 0;
+        ans = Math.floor(Math.random() * 100) + 1;
+        document.getElementById("new").appendChild(para);     // Append <p> to <div> with id="myDIV"
+    }
+    else if(c == ans){
+    
+        var para = document.createElement("p"); 
+                        // Create a <p> element
+        para.innerHTML = "Congratulation You Win!!!!!! ...Starting new Game";             // Insert text
+        temp = 0;
+        ans = Math.floor(Math.random() * 100) + 1;
+        document.getElementById("new").appendChild(para);     // Append <p> to <div> with id="myDIV"
+
+    }else if(c < ans){
+        var para = document.createElement("p"); 
+        // Create a <p> element
+        para.innerHTML = "Your Number is less than the answer. Your current number is " + c;                // Insert text
+        document.getElementById("new").appendChild(para);     // Append <p> to <div> with id="myDIV"
+        temp ++;
+    }else{
+        var para = document.createElement("p"); 
+        // Create a <p> element
+        para.innerHTML = "Your Number is greater than the answer. Your current number is " + c;             // Insert text
+        document.getElementById("new").appendChild(para);     // Append <p> to <div> with id="myDIV"
+        temp ++;
+    }
+
+
+    return false;
+}
